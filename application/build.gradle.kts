3ef27a3a28996777d6bd9b00efd42505644f3651
Paul <paulsegfault@gmail.com>
2020-08-21 01:09:01 +0300

    Fixed retrofit building issues

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,6 +1,6 @@
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
-  implementation(project(":clients"))
+  implementation(project(":client"))
   implementation("org.springframework.boot:spring-boot-starter-web")
 }
